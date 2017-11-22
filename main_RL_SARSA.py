8a3675e24a221bc84353c8d89a2d59df8d68cd5a
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2017-11-22 18:20:50 +0200

    final version of submission

--- main_RL_SARSA.py
+++ main_RL_SARSA.py
@@ -13,7 +13,7 @@ class Agent(object):
     '''
     This class is responsible for our robot representation
     '''
-    def __init__(self, robot, alpha, gamma, epsilon, q_init):
+    def __init__(self, robot, alpha, gamma,  q_init, softmax_temperature = 1):
         self.robot = robot
         self.num_actions_0 = 3
         self.num_actions_1 = 3
@@ -32,30 +32,29 @@ class Agent(object):
             np.linspace(0, np.pi/2, self.num_states_0, endpoint=False)[1:],
             np.linspace(0, np.pi/2, self.num_states_1, endpoint=False)[1:]]
 
-        self.q_table = np.full((self.num_states, self.num_actions), q_init) #todo CHECK IT
+        self.q_table = np.full((self.num_states, self.num_actions), q_init)
         self.alpha = alpha      # learning rate
         self.gamma = gamma      # discount factor
-        self.epsilon = epsilon
+        self.softmax_temperature = softmax_temperature
 
     def choose_action(self, state):
-        # ToDo: implement choose action function
-        # TODO implement here serch policy
         r = random.random()
 
-        if r > self.epsilon:
-            action = np.argmax(self.q_table[state])
-            # print(self.q_table[state])
-            # print( 'max ' + str(self.q_table[state].argmax() ) + str(self.q_table[ self.q_table[state].argmax() ] ) )
+        #softmax exploration:
 
-            #TODO error here
-         #   action = random.choice(range(len(self.q_table[state])))
+        probs  = np.zeros(len(self.q_table[state]))
+        temperatured_probs = np.zeros(len(self.q_table[state]))
+        total_temp_probs = 0
+        actions = self.q_table[state]
 
-            #greede select
-            # pass
-        else:
-            action = random.choice( range ( len ( self.q_table[state]) ) )
-            # pass
-            #explore select:
+        for i in range (len(actions)):
+            temperatured_probs[i] = e ** (actions[i] / self.softmax_temperature)
+            total_temp_probs += temperatured_probs[i]
+
+        for i in range (len(actions)):
+            probs[i] = temperatured_probs[i] / total_temp_probs
+
+        action = np.random.choice( range (len (self.q_table[state])) , p=probs) #creating indexes array and making choice according to softmax
 
         return action
 
@@ -90,14 +89,15 @@ class Agent(object):
         self.state = state_new
         self.position = position_new
 
+
     def update_q(self, state, action, reward, state_new):
-        #state_new = state
-        # old_state = self.q_table[state][action]
-        q_sa = self.q_table[state, action]
-        self.q_table[state, action] = q_sa + self.alpha * (reward + self.gamma * np.max(self.q_table[state_new]) - q_sa)
-        # self.q_table[state][action] += self.alpha * (reward + gamma * np.argmax(self.q_table[state_new]) - self.q_table[state][action])
-        #ToDo: implement q-value update
-		# pass
+
+        #SARSA update policy
+        new_step_action = self.choose_action(state_new)
+
+        self.q_table[state][action] += self.alpha * (reward + gamma * self.q_table[state_new][new_step_action] - self.q_table[state][action])
+
+
 
     def initialize_episode(self):
         self.robot.restart_simulation()
@@ -165,35 +165,36 @@ if __name__ == '__main__':
     print "Ping time: %f" % (sec + msec / 1000.0)
 
     robot = Robot(client_id)
-    #learning rate alpha
-    alpha = 1 # as we have to init with some values,
+    # learning rate alpha
+    alpha = 1  # as we have to init with some values,
     # but don't know which exactly to chose therfore we taking learning rate as hight as possible
     # to 'intialize' transitons with revard function's values
     # and rapidly decrease it to feasible values to preserve convergence
-    final_alpha = 0.2
+    final_alpha = 0.1
+    alpha_gradient = 0.8
 
     num_episodes = 20
     len_episode = 150
     return_history = []
 
     #  discount factor gamma,
-    gamma = 0.7 # decrease of weight for unpleasant values
-    # have to preserve not too hight, to prevent of learning mistakes like a positive revard
+    gamma = 0.6  # decrease of weight for unpleasant values
     #  epsilon-greedy rate epsilon
-    epsilon = 0.3 # explore / exploit partition, lower part - for explore
-    epsilon_end = 0.005 #final epsillon value to make our crawler move greedily in at the last iterations
-    epsilon_decrease_rate = (epsilon_end / epsilon) ** (1.0/num_episodes)
 
     #  initil value of q-table q_init
-    q_init = 0.0 # ignore init policy, instead rew
-    agent = Agent(robot, alpha, gamma, epsilon, q_init)
+    q_init = 0.0  # ignore init policy, instead rew
 
-
+    softmax_temperature = 1
+    final_softmax_temperature = 0.01 # for final greedy presentation
+    temperature_cooldown_gradient = final_softmax_temperature / softmax_temperature **  ( 1.0 / num_episodes)
+    agent = Agent(robot, alpha, gamma,  q_init, softmax_temperature)
 
 
     try:
         for episode in range(num_episodes):
             print "start simulation # %d" % episode
+
+            # print (agent.q_table ) TODO CHECK HOW IS GOING
 
             with contexttimer.Timer() as timer:
                 agent.initialize_episode() #nothing to place here, just init values
@@ -212,9 +213,8 @@ if __name__ == '__main__':
             position = body_trajectory[-1]
             return_history.append(position[0])
             q_table = agent.q_table
-            agent.epsilon = agent.epsilon * epsilon_decrease_rate
-            agent.alpha = final_alpha + agent.alpha * 0.7
-            print( "EPSILLON" + str (agent.epsilon) )
+            agent.alpha = final_alpha + agent.alpha *  alpha_gradient
+            agent.softmax_temperature = final_softmax_temperature + softmax_temperature * temperature_cooldown_gradient
 
           #  plot(body_trajectory, joints_trajectory, return_history, q_table)
             print
